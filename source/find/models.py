8d88c154859cbae74b097328e96ee6e910b9b05f
Ahnaf <ahnaf.ismat@northsouth.edu>
2021-05-05 18:14:25 +0600

    reset find app

--- source/find/models.py
+++ source/find/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Skill(models.Model):
-    name = models.CharField(max_length=15)
-
-    def __str__(self):
-        return f"{self.name}"
