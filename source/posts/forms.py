40c4a5c23c6b5e3a4213872f72ad2dfb480637f3
Simon-48 <80439125+Simon-48@users.noreply.github.com>
2021-05-06 14:08:36 +0600

    Update forms.py
    Update sphinx commenting.

--- source/posts/forms.py
+++ source/posts/forms.py
@@ -6,7 +6,7 @@ class PostModelForm(forms.ModelForm):
     This class takes a helper class ModelForm that 
     lets you create a Form class from a Django model.
     
-	"""
+    """
     content = forms.CharField(widget=forms.Textarea(attrs={'rows':2}))
     class Meta:
         model = Post
@@ -17,7 +17,7 @@ class CommentModelForm(forms.ModelForm):
     This class takes a helper class ModelForm that 
     lets you create a Form class from a Django model.
 
-	"""
+    """
     body = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Add a comment...'}))
     class Meta:
         model = Comment
