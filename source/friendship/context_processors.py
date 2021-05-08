2de9eb166694a58948545b8d0a74c7bbeda6b313
Your Name <you@example.com>
2021-05-08 10:31:23 +0600

    building models , database tables  for friendship

--- source/friendship/context_processors.py
+++ source/friendship/context_processors.py
@@ -1,1 +1,44 @@
-context_processors.py
+from .models import Profile, Relationship
+
+
+
+    '''
+    This method will pass the avatar of requested user profile.
+
+    '''
+
+
+
+
+
+def profile_pic(request):
+
+    '''
+    This method will pass the avatar of requested user profile.
+     :param name: request - it generates the response(HTTP)
+    :param type: HttpResponse
+    :return: returns an object
+
+    '''
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        pic = profile_obj.avatar
+        return {'picture':pic}
+    return {}
+
+
+
+def invatations_received_no(request):
+
+    '''
+    This method will count the number of invitation received.
+    :param name: request - it generates the response(HTTP)
+    :param type: HttpResponse
+    :return: returns a number
+
+    '''
+    if request.user.is_authenticated:
+        profile_obj = Profile.objects.get(user=request.user)
+        qs_count = Relationship.objects.invatations_received(profile_obj).count()
+        return {'invites_num':qs_count}
+    return {}
--- source/friendship/models.py
+++ source/friendship/models.py
@@ -1,12 +1,16 @@
-
-
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth.models import User
 from django.db.models import Q
 
-# Create models here.
+
 class ProfileManager(models.Manager):
+"""
+    This class contains the essential fields and behaviors of the data we’re storing. 
+    Each model maps to a single database table.
+    
+    This class is used to create a database table containg those attributes.
+    """
 
     def get_all_profiles_to_invite(self, sender):
         profiles = Profile.objects.all().exclude(user=sender)
@@ -33,19 +37,31 @@ class ProfileManager(models.Manager):
         profiles = Profile.objects.all().exclude(user=me)
         return profiles
 
-
+'''these functions defines one will be sent to databse and other parameter will be for us to
+ see in the browser'''
 STATUS_CHOICES = (
     ('send', 'send'),
     ('accepted', 'accepted')
 )
 
 class RelationshipManager(models.Manager):
+   ''' This class contains the essential fields and behaviors of the data you’re storing. 
+        Each model maps to a single database table.
+    
+        This class is used to create a database table containg those attributes.'''
+
     def invatations_received(self, receiver):
         qs = Relationship.objects.filter(receiver=receiver, status='send')
         return qs
 
 
 class Relationship(models.Model):
+
+   '''This class contains the essential fields and behaviors of the data you’re storing. 
+      Each model maps to a single database table.
+     
+      This class is used to create a database table containg those attributes.'''
+
     sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
@@ -55,4 +71,11 @@ class Relationship(models.Model):
     objects = RelationshipManager()
 
     def __str__(self):
+      '''  """
+  This method will return user's username and time it user was created
+  :param name: self - used to access the attributes and methods of the class in python
+  :param type: reference
+  :return: str
+        
+  """'''
         return f"{self.sender}-{self.receiver}-{self.status}"
