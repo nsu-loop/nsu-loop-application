456deb5f54a10e1a56bd8e3d639746287397a304
Your Name <you@example.com>
2021-05-09 23:06:12 +0600

    Unit test done

--- source/chats/tests/test_views.py
+++ source/chats/tests/test_views.py
@@ -8,16 +8,25 @@ from chats.models import Chat
 
 @pytest.fixture(autouse=True)
 def enable_db_access(db):
-    
+    """
+    Global DB access to all tests.
+    :param db:
+    :return:
+    """
     pass
 
 
 class TestChatBase:
-    
+    """
+    This class contains all the required fixtures for the Chat test suit
+    """
 
     @pytest.fixture
     def from_user(self, ):
-       
+        """
+        creates a test user
+        :return: the object of the test user
+        """
         username = f"user{random.randint(9, 99999)}"
         return User.objects.create(
             username=username
@@ -25,7 +34,10 @@ class TestChatBase:
 
     @pytest.fixture
     def to_user(self, ):
-        
+        """
+        creates a test user
+        :return: the object of the test user
+        """
         username = f"user{random.randint(9, 99999) + 1}"
         return User.objects.create(
             username=username
@@ -33,7 +45,12 @@ class TestChatBase:
 
     @pytest.fixture
     def chat(self, from_user, to_user):
-      
+        """
+        creates a chat entry in the database
+        :param from_user: the sender person of the chat
+        :param to_user: the receiver person of the chat
+        :return: the chat object
+        """
 
         return Chat.objects.create(
             from_user=from_user,
@@ -43,10 +60,17 @@ class TestChatBase:
 
 
 class TestChatList(TestChatBase):
-    
+    """
+    Inside this class, all the test cases related to chat will be tested.
+    """
 
     def test_empty_chat_list(self, from_user, to_user):
-       
+        """
+        Testing the empty chat list's behaviour
+        :param from_user: the sender user
+        :param to_user: the receiver user
+        :return:
+        """
         assert not Chat.objects.filter(
             id=from_user.id
         )
@@ -56,7 +80,11 @@ class TestChatList(TestChatBase):
         )
 
     def test_chat_list(self, chat):
-        
+        """
+        Testing the populated chat list's behaviour
+        :param chat: the chat object of both from_user and to_user
+        :return:
+        """
         assert chat
         assert Chat.objects.filter(
             id=chat.from_user.id
